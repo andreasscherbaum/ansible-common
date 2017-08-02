63fe522f08f5c9235e2aa3107cd48dd9f4e8cb83
Andreas Scherbaum <andreas@scherbaum.biz>
2017-08-02 20:22:35 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ansible-common
+
 Ansible Playbook which does basic configuration on a new host
+
+## Main usage
+
+That is primarily my own "common" set of instructions how to install a new host. But feel free to look around ...
+
---
+++ templates/etc_profile.d_ads.sh
@@ -0,0 +1,18 @@
+HISTCONTROL=ignoredups
+export HISTCONTROL
+HISTSIZE=100000
+export HISTSIZE
+LC_TIME=de_DE.UTF-8
+export LC_TIME
+
+alias unzip='unzip -L'
+alias pico='nano -w'
+alias bash='bash --login'
+alias c='clear'
+
+# Midnight Commander
+COLORTERM=1
+export COLORTERM
+
+PAGER=less
+export PAGER
